d02388547998cc0a18df6c59755ea0f631bbb436
gouthami <gouthaminersu@gmail.com>
2026-09-23 12:35:04 +0530

    stack

--- stack.py
+++ stack.py
@@ -1,78 +1,72 @@
-class Node:
-  def __init__(self,data):
-    self.data=data
-    self.next=None
-class LinkedList():
-  def __init__(self,data):
-    self.head=None
-    self.size=0
-  def add(self,data):
-    if self.head==None:
-      self.head=Node(data)
-      self.size+=1
-      return
-    cN=self.head
-    while cN.next is not None:
-      cN=cN.next
-    cN.next=Node(data)
-    self.size+=1
-  def traverse(self):
-    if self.head==None:
-      print()
-      return
-    cN=self.head
-    while cN.next is not None:
-      print(cN.data,end='->')
-      cN=cN.next
-    print(cN.data)
-  def search(self,data):
-    if self.head==None:
-      print("no elements in LL")
-      return
-    ind=0
-    cn=self.head
-    while cn.next is not None:
-      if cn.data==data:
-       print(f'element {data} is found at {ind} index')
-       return
-      cn=cn.next
-      ind+=1
-    if cn.data==data:
-      print(f'element {data} is found at {ind} index')
-      return
-    print('element not found')  
-  def len(self):
-    return self.size
-  def begin(self,data):
-    node=Node(data)
-    node.next=self.head
-    self.head=node
-  def delbegin(self):
-    if self.head==None:
-      return
-    else:
-      self.head=self.head.next
-  def delLast(self):
-    if self.head==None:
-     return
-    cN=self.head
-    while cN.next is not None:
-      cN=cN.next
-    cN.next=None
-    self.size-=1
-      
+class stack:
+    def __init__(self):
+        self._a = []
+        self._top = None
 
-ll=LinkedList(10)
-ll.add(20)
-ll.add(30)
-ll.add(40)
-ll.add(50)
-#print(ll.head.next.data)
-ll.traverse()
-ll.search(30)
-print(ll.len())
-ll.begin(100)
-ll.traverse()
-ll.delbegin()
-ll.traverse()
-ll.delLast()
+    def push(self, data):
+        if self._top is None:
+            ar = [0]
+            self._top = 0
+            ar[self._top] = data
+            self._a = ar
+        else:
+            ar = [0 for i in range(self._top + 2)]
+
+            for i in range(self._top + 1):
+                ar[i] = self._a[i]
+
+            ar[-1] = data
+            self._top += 1
+            self._a = ar
+
+    def peek(self):
+        if self._top is None:
+            return "we dont have any elements"
+
+        return self._a[self._top]
+
+    def apnd(self, data):
+        ar = [0 for i in range(self._top + 2)]
+
+        for i in range(self._top + 1):
+            ar[i] = self._a[i]
+
+        ar[-1] = data
+        self._top += 1
+        self._a = ar
+
+    def pop(self):
+        if self._top is None:
+            return "we dont have any elements"
+
+        data = self._a[self._top]
+        self._top -= 1
+
+        if self._top == -1:
+            self._a = []
+            self._top = None
+        else:
+            ar = [0 for i in range(self._top + 1)]
+
+            for i in range(self._top + 1):
+                ar[i] = self._a[i]
+
+            self._a = ar
+
+        return data
+
+
+stack = stack()
+
+stack.push(10)
+stack.push(20)
+
+print(stack.peek())
+
+stack.apnd(30)
+print(stack.peek())
+
+removed = stack.pop()
+print("removed:", removed)
+
+print(stack.peek())
